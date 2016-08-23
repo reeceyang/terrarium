42f458c9d951dc79ed785ee72dedf3b1a5a3fcc4
Reece <reeceyang@live.com>
2016-08-23 13:11:25 -0500

    Create directions.js

---
+++ directions.js
@@ -0,0 +1,9 @@
+var directions = new Dictionary(
+  {"n":  new Point( 0, -1),
+   "ne": new Point( 1, -1),
+   "e":  new Point( 1,  0),
+   "se": new Point( 1,  1),
+   "s":  new Point( 0,  1),
+   "sw": new Point(-1,  1),
+   "w":  new Point(-1,  0),
+   "nw": new Point(-1, -1)});  // The direction of success
