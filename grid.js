7bac91c15dc1bd20ca77cf9d61a1c10d4b34a064
Reece <reeceyang@live.com>
2016-08-23 12:44:47 -0500

    Update grid.js
    Added each function to the prototype of Grid.

--- grid.js
+++ grid.js
@@ -17,3 +17,11 @@ Grid.prototype.moveValue = function(from, to){
   this.setValueAt(to, this.valueAt(from));
   this.setValueAt(from, undefined);
 };
+Grid.prototype.each = function(action) {
+  for (var y = 0; y < this.height; y++) {
+    for (var x =0; x < this.width; x++) {
+      var point = new Point(x, y);
+      action(point, this.valueAt(point));
+    }
+  }
+};
