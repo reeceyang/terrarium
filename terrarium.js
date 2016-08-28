5c3bf64c40b3124a19707db833f7a09f6fce205d
Reece <reeceyang@live.com>
2016-08-28 13:31:35 -0500

    Update terrarium.js

--- terrarium.js
+++ terrarium.js
@@ -21,3 +21,13 @@ function Terrarium(plan) {
   this.grid = grid;
 }
   
+Terrarium.prototype.toString = function() {
+  var characters = [];
+  var endOfLine = this.grid.width - 1;
+  this.grid.each(function(point, value) {
+    characters.push(characterFromElement(value));
+    if (point.x == endOfLine)
+      characters.push("\n");
+  });
+  return characters.join("");
+}
