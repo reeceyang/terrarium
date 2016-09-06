2ad2ab453e7e8ec6c7f437d8cbf9f1132c4599e7
Reece Yang <reeceyang@live.com>
2016-09-06 09:53:47 -0500

    Update foreachin.js

--- foreachin.js
+++ foreachin.js
@@ -1,6 +1,7 @@
 function forEachIn(object, action) {
   for (var property in object) {
-    if (Object.prototype.hasOwnProperty.call(object, property))
+    if (Object.prototype.hasOwnProperty.call(object, property)){
       action(property, object[property]);
+    }
   }
 }
